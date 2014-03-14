8704c9e0c0dfa1ffc17b6f8f9b02fe40b196eef4
Arne Brodowski <mail@arnebrodowski.de>
2014-03-14 15:58:10 +0100

    Make autocomplete-view url dynamic using 'reverse' and make Django-Admin integraition easier by falling back to django.jQuery, if jQuery is not available

--- taggit_live/forms.py
+++ taggit_live/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
 from taggit.forms import TagField
 from taggit.utils import edit_string_for_tags
 
@@ -16,7 +17,8 @@ class TaggitLiveWidget(forms.TextInput):
         if value is not None and not isinstance(value, basestring):
             value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
         rendered = super(TaggitLiveWidget, self).render(name, value, attrs)
-        js = u'<script type="text/javascript">jQuery(function() { $("#%s").taggit_live(); });</script>' % (attrs['id'])
+        url = reverse("taggit_autocomplete_list")
+        js = u'<script type="text/javascript">(function($) { $("#%s").taggit_live({callback: "%s"}); })(jQuery || django.jQuery);</script>' % (attrs['id'], url)
         return rendered + mark_safe(js)
 
 
